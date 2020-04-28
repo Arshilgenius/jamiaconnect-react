f99f047f54b97105f12f21dd7bb93bd336e9fb5c
arshilgenius <arshilgenius@gmail.com>
2020-04-29 04:56:47 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import axios from "axios";
 class App extends Component {
 	state = {
 		posts: [],
+		title: null,
+		desc: null,
 	};
 	componentDidMount() {
 		axios
@@ -15,9 +17,48 @@ class App extends Component {
 			});
 	}
 
+	onSubmit = () => {
+		const val = {
+			title: this.state.title,
+			description: this.state.desc,
+			key: "",
+			comments: Math.random() * 10,
+		};
+
+		console.log(val);
+		axios
+			.post("https://jamia-connect-7679c.firebaseio.com/forums.json", val)
+			.then((resp) => {
+				console.log(resp);
+			});
+	};
+
+	handleChange = (e) => {
+		this.setState({
+			[e.target.name]: e.target.value,
+		});
+	};
+
 	render() {
 		return (
 			<div className="App">
+				<form>
+					<input
+						type="text"
+						name="title"
+						value={this.state.name}
+						onChange={(e) => this.handleChange(e)}
+					/>
+					<input
+						type="text"
+						name="desc"
+						value={this.state.name}
+						onChange={(e) => this.handleChange(e)}
+					/>
+					<button type="button" onClick={this.onSubmit}>
+						Save
+					</button>
+				</form>
 				{this.state.posts.map((post) => {
 					return (
 						<div className="card">
